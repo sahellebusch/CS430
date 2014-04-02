9daf7084262e046947852035dd17887b71ebfe98
Hellebusch <sahellebucsh@gmail.com>
2014-04-02 18:29:48 -0500

    changed to GET response

--- php/examples/json_example.php
+++ php/examples/json_example.php
@@ -1,13 +1,49 @@
 <?php
-//Get and convert
-$JSON = $_POST['data'];
-$JSONarray = json_decode($JSON);
-var_dump($JSONarray);
+if(!isset($_GET['data'])) {
+    header('400 bad request');
+    exit;
+}
+else {
+    try {
+ 
+    // Database login
+    $dbuser = 'jpf7324';
+    $dbpass = 'oxaetoht';
+    
+    //Connect to DB
+    $db = new PDO("mysql:host=mysql.truman.edu;dbname=jpf7324CS430;charset=utf8", $dbuser, $dbpass); 
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//Increment
-$JSON[0][1].age++;
-var_dump($JSON);
+    $stmt = $db->prepare("INSERT INTO `person`(username, banner, phone, date_joined, first_name, last_name) 
+             VALUES (:username, :banner, :phone, :date_joined, :first_name, :last_name)");
+        $stmt->bindValue(':username', 'TEST');
+        $stmt->bindValue(':banner', 123456789);
+        $stmt->bindValue(':phone', 1234567987);
+        $stmt->bindValue(':date_joined', 2014-02-02);
+        $stmt->bindValue(':first_name', 'TOMMY');
+        $stmt->bindValue(':last_name', 'TESTER');
+        $stmt->execute();
+    }  catch(PDOException $e) {
+        
+        echo 'error: ' . $e->getMessage();   
+        
+    }
+}
 
-//Convert to JSON
-echo json_encode($JSONarray);
+
+
+
+
+////Get and convert
+//if(isset($_POST['data'])) {
+//    //Get
+//    $JSON = $_POST['data'];
+//    $jsonData = json_decode($JSON);
+//    var_dump($jsonData);
+//    //Manipulate (increment age)
+//    $JSON[0][1].age++;
+//    var_dump($JSON);
+//}
+////Convert to JSON and give back
+//echo json_encode($jsonData);
 ?>
