618a2f25e09735d1ee4d59fca94eea9ccc4522e9
John Foley <johnfoley3@gmail.com>
2014-04-03 11:20:58 -0500

    preparing to accept data from php ajax call

--- app/js/controllers/app.js
+++ app/js/controllers/app.js
@@ -10,28 +10,36 @@ This web app handles logistics for Truman State Student Government
 var app = angular.module("stugovapp", []);
 
 app.controller("indexCtr", function( $scope, $http ) {
+    
     $scope.persons = [
         {"name":"john", "age":21},
         {"name":"sean", "age":22}];
     
-    $scope.url = 'search.php'; // The url of our search
-         
-    // The function that will be executed on button click (ng-click="search()")
-    $scope.search = function() {
+    /*
+    $scope.status = "Good so far";
+    
+    $scope.result = $scope.persons;
+    
+    // The url of the AJAX call
+    $scope.url = '../../php/search.php'; 
+      
+    
+    // The function that will be executed on button click (ng-click="insertPerson()")
+    $scope.insertPerson = function() {
          
         // Create the http post request
         // the data holds the keywords
         // The request is a JSON request.
-        $http.post($scope.url, { "data" : $scope.keywords}).
-        success(function(data, status) {
+        $http.post($scope.url, { "data" : $scope.persons})
+        .success(function(data, status) {
             $scope.status = status;
             $scope.data = data;
             $scope.result = data; // Show result from server in our <pre></pre> element
         })
-        .
-        error(function(data, status) {
+        .error(function(data, status) {
             $scope.data = data || "Request failed";
             $scope.status = status;         
         });
     };
+    */
 });
