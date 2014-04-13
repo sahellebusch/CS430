e9bc8c91e30b8d01ab454433d026bc639e31b8bf
John Foley <johnfoley3@gmail.com>
2014-04-12 22:13:20 -0500

    not working

--- app/js/app.js
+++ app/js/app.js
@@ -15,28 +15,28 @@ var stugovApp = angular.module('stugovApp', [
 // Configure routing for each partial page
 stugovApp.config(['$scope', '$routeProvider', 'personAjax',
   function ($scope, $routeProvider, personAjax) {
-    $routeProvider
-        .when('/index', {
-            templateUrl: 'app/js/partials/front.html',
-            controller: 'frontCtr'
-        })
-        .when('/person', {
-            templateUrl: 'app/js/partials/person.html',
-            controller: 'personCtr'
-        })
-        .when('/person/:pid', {
-            templateUrl: 'app/js/partials/personDetail.html',
-            controller: 'personDetailCtr'
-        })
-        .when('/person/edit/:pid', {
+        $routeProvider
+            .when('/index', {
+                templateUrl: 'app/js/partials/front.html',
+                controller: 'frontCtr'
+            })
+            .when('/person', {
+                templateUrl: 'app/js/partials/person.html',
+                controller: 'personCtr'
+            })
+            .when('/person/:pid', {
+                templateUrl: 'app/js/partials/personDetail.html',
+                controller: 'personDetailCtr'
+            })
+            .when('/person/edit/:pid', {
                 templateUrl: 'app/js/partials/personDetailEdit.html',
                 controller: 'personDetailEditCtr'
             })
             .when('/meetings', {
-    templateUrl: 'app/js/partials/meeting.html',
-    controller: 'meetingCtr'
-})
+                templateUrl: 'app/js/partials/meeting.html',
+                controller: 'meetingCtr'
+            })
             .otherwise({
-    redirectTo: '/index'
-});
+                redirectTo: '/index'
+            });
 }]);
