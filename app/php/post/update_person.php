38b77df46d577b6d758e7747d1db45b507b245b4
Hellebusch <sahellebucsh@gmail.com>
2014-04-16 14:43:27 -0500

    changes to editperson php, not done

--- app/php/post/update_person.php
+++ app/php/post/update_person.php
@@ -33,16 +33,23 @@ try {
     // Prepare Statement
     $stmt = $db->prepare("UPDATE person SET username = :username, banner = :banner, phone = :phone, date_joined = :date_joined,                         first_name = :first_name, last_name = :last_name, WHERE p_id = :p_id 
         VALUES (:p_id, :username, :banner, :phone, :date_joined, :first_name, :last_name)");
-    // Convert date joined to SQL format
-    $date_joined_SQL_format = date('Y-m-d', strtotime(str_replace('-', '/', $person_data[3]['date_joined'])));
+    // Check to see if date is in correct format, bind.
+    // If not, convert to correct format and bind.
+    $date_joined = $person_data['date_joined'];
+    echo var_dump($date_joined);
+    if(preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $date_joined)){ 
+        $stmt->bindValue(':date_joined', $date_joined);
+    }else{ 
+        $date_joined = date('Y-m-d', strtotime(str_replace('-', '/', $date_joined)));
+        $stmt->bindValue(':date_joined', $date_joined);
+    }
     // Bind values
-    $stmt->bindValue(':username', $person_data[0]['username']);
-    $stmt->bindValue(':banner', $person_data[1]['banner']);
-    $stmt->bindValue(':phone', $person_data[2]['phone']);
-    $stmt->bindValue(':date_joined', $date_joined_SQL_format);
-    $stmt->bindValue(':first_name', $person_data[4]['first_name']);
-    $stmt->bindValue(':last_name', $person_data[5]['last_name']);
-    $stmt->bindValue(':p_id', $person_data[6]['p_id']);
+    $stmt->bindValue(':username', $person_data['username']);
+    $stmt->bindValue(':banner', (int)$person_data['banner']);
+    $stmt->bindValue(':phone', (int)$person_data['phone']);
+    $stmt->bindValue(':first_name', $person_data['first_name']);
+    $stmt->bindValue(':last_name', $person_data['last_name']);
+    $stmt->bindValue(':p_id', (int)($person_data['p_id']));
     // Execute SQL
     $stmt->execute();
     // Report Success
