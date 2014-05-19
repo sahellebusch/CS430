c038b4eed57f7c3c006bcf4dc380a9940397f33a
sahellebusch <sahellebusch@gmail.com>
2014-05-19 18:22:07 -0500

    tests for Validator working!

--- app/php/class_files/Validator.php
+++ app/php/class_files/Validator.php
@@ -2,7 +2,7 @@
 
 /*
  * Author: Sean Hellebusch
- * Date: 5.6.14
+ * Date: 5.19.14
  * PHP class to abstract data validations
  */
 
@@ -12,30 +12,28 @@ class Validator {
     function validateDate($date_joined) {
         if((preg_match('/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/', $date_joined))) {
             list( $y, $m, $d ) = preg_split( '/[-\.\/ ]/', $date_joined );
-            return (checkdate( $m, $d, $y ) and $y <= date('Y'));
+            return (checkdate( $m, $d, $y ) && ($y >= date('Y') - 2) && ($y <= date('Y')));
         } else {
             // wrong format, convert to correct format
             $date_joined = date('Y-m-d', strtotime(str_replace('-', '/', $date_joined)));
             list( $y, $m, $d ) = preg_split( '/[-\.\/ ]/', $date_joined );
-            return (checkdate( $m, $d, $y ) and $y <= date('Y'));
+            return (checkdate( $m, $d, $y ) && ($y >= date('Y') - 2) && ($y <= date('Y')));
             }
     }
 
     // Validates banner is no longer than 10 ints long.
     function validateBanner($banner) {
-        return(strlen($banner) <= 10);
+        return(is_int($banner) 
+               && !(strlen($banner) == 0) 
+               && strlen($banner) <= 10);
     }
 
-    // Validates phone number
+    
     function validatePhone($phone) {
-    $numbersOnly = ereg_replace("[^0-9]", "", $phone);
-    $numberOfDigits = strlen($numbersOnly);
-    if ($numberOfDigits == 7 or $numberOfDigits == 10) {
-        return true;
-    } else {
-        return false;
+        //eliminate every char except 0-9
+        $phone = $justNums = str_replace('-', '', $phone);
+        return((strlen($phone) == 10) or (strlen($phone) == 7));
     }
-} 
     
 }
 
--- app/php/tests/testValidations.php
+++ app/php/tests/testValidations.php
@@ -1,4 +1,11 @@
 <?php 
+
+/*
+ * Author: Sean Hellebusch
+ * Date: 5.19.14
+ * phpUnit test class for Validator
+ */
+
 require_once "../class_files/Validator.php";
     
 class TestValidations extends PHPUnit_framework_TestCase {
@@ -15,7 +22,7 @@ class TestValidations extends PHPUnit_framework_TestCase {
     }
     
     public function testValidateBannerNonNumber() {
-        $this->assertFalse($this->val->validateBanner('aft156'));
+        $this->assertFalse($this->val->validateBanner('123zzz456'));
     }
     
     public function testValidateBannerBigNumber() {
@@ -23,29 +30,58 @@ class TestValidations extends PHPUnit_framework_TestCase {
     }
     
     //////////// Test date validation method ////////////
-    public function testValidateDateGoodDate() {
-        $this->assertTrue($this->val->validateDate(2014-02-02));
+    public function testValidateDateGoodDateSQLFormat() {
+        $this->assertTrue($this->val->validateDate('2014-02-02'));
     }
     
-    public function testValidateDateBadDay() {
-       $this->assertFalse($this->val->validateDate(2014-02-100));
+    public function testValidateDateGoodDateNonSQLFormat() {
+        $this->assertTrue($this->val->validateDate('02-02-2014'));
     }
     
-    public function testValidateDateBadMonth() {
-       $this->assertFalse($this->val->validateDate(2014-100-02));
+    public function testValidateDateBadDaySQLFormat() {
+       $this->assertFalse($this->val->validateDate('2014-02-38'));
+    }
+    
+    public function testValidateDateBadDayNonSQLFormat() {
+        $this->assertFalse($this->val->validateDate('38-02-2014'));
+    }
+    
+    public function testValidateDateBadMonthSQLFormat() {
+       $this->assertFalse($this->val->validateDate('2014-38-02'));
+    }
+    
+    public function testValidateBadMonthNonSQLFormat() {
+        $this->assertFalse($this->val->validateDate('02-38-2014'));
     }
     
     public function testValidateDateBadYear() {
-       $this->assertFalse($this->val->validateDate(2000000-02-02));
+       $this->assertFalse($this->val->validateDate('2000000-02-02'));
     }
     
     //////////// Test date validation method ////////////
-    public function testValidatePhoneGoodNumber() {
-        $this->assertTrue($this->val->validatePhone(123-456-7890));
+    public function testValidatePhoneGoodNumberWithHyphens10Digits() {
+        $this->assertTrue($this->val->validatePhone('111-111-1111'));
     }
     
-    public function testValidatePhoneTooManyDigits() {
-        $this->assertFalse($this->val->validatePhone(1234567890));
+    public function testValidatePhoneGoodNumberNoHyphens10Digits() {
+        $this->assertTrue($this->val->validatePhone(1234567890));
+    }
+    
+    public function testValidatePhoneGoodNumberWithHyphens7Digits() {
+        $this->assertTrue($this->val->validatePhone('111-1111'));
+    }
+    
+    public function testValidatePhoneGoodNumberNoHyphens7Digits() {
+        $this->assertTrue($this->val->validatePhone(1234567));
+    }
+    
+    public function testValidatePhoneTooManyDigitsWithHyphens() {
+        $this->assertFalse($this->val->validatePhone('123-456-7890123456789'));
+    }
+    
+    
+    public function testValidatePhoneTooManyDigitsNoHyphens() {
+        $this->assertFalse($this->val->validatePhone(1234567890123456789));
     }
     
     public function tearDown() {
